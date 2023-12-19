b90d93e554907a1f7859175cd33db84a077c5267
fjkk <fujikake.takayoshi@gmail.com>
2023-12-19 18:10:45 +0900

    initial commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "../css/app.css";
+
+const root = createRoot(document.getElementById("app"));
+
+root.render(
+  <>
+    <h1>Hello world222!</h1>
+  </>
+);
